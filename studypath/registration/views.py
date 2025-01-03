7c614e6b3b689d311027c34b18b3e83ba2b4d311
Jeff Ivan <jeffivanbiosanomayor@gmail.com>
2025-01-03 22:41:22 +0800

    added the backend of the register form

--- studypath/registration/views.py
+++ studypath/registration/views.py
@@ -19,5 +19,22 @@ def login(request):
 
 def register(request):
     if request.method == "POST":
-        pass
+        first_name = request.POST('first_name')
+        last_name = request.POST('last_name')
+        email = request.POST('email')
+        studentId = request.POST('studentID')
+        password = request.POST('password')
+        password2 = request.POST('password2')
+
+        if len(studentId) == 0 or len(password) >= 10:
+            messages.error(request, 'Invalid student ID')
+            return redirect('register')
+        elif len(password) < 8:
+            messages.error(request, 'Password must be at least 8 characters')
+            return redirect('register')
+        elif password != password2:
+            messages.error(request, 'Password does not match')
+            return redirect('register')
+        else:
+            pass
     return render(request, 'register.html')

690dfade65765154943edd1af3377b9aebcea41a
Jeff Ivan <jeffivanbiosanomayor@gmail.com>
2025-01-03 22:35:45 +0800

    updates views.py

--- studypath/registration/views.py
+++ studypath/registration/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 
 def login(request):
     if request.method == "POST":
-        pass
+        studentId = request.POSt('studentID')
+        password = request.POST('password')
+
+        if len(studentId) == 0 or len(password) >= 10:
+            messages.error(request, 'Invalid student ID')
+            return redirect('login')
+        elif password == "":
+            messages.error(request, 'Fill the password field')
+            return redirect('login')
+        else:
+            pass
     return render(request, 'login.html')
 
 def register(request):
